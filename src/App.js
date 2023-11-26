c6241fe65b5804799307b2accaaf3c826611c0b0
GabrielMelhem <gabrielmelhem@gmail.com>
2023-11-26 22:39:45 +0100

    Form Addes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { Weather } from "./Weather";
+import { Form } from "./Form";
+import "./styles.css";
+import React, { useState } from "react";
 
-function App() {
+//
+const API_KEY = "27f4050de5eaa18b63fe814a20d92367";
+
+export default function App() {
+  const [weatherData, setWeatherData] = useState({
+    tempreature: "",
+    city: "",
+    country: "",
+    humidity: "",
+    description: "",
+    error: ""
+  });
+  const getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    //console.log(city, country);
+
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}%2C${country}&appid=${API_KEY}`
+    );
+    const data = await response.json();
+    //console.log(data);
+
+    if (city && country) {
+      setWeatherData({
+        tempreature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+    } else {
+      setWeatherData({
+        tempreature: "",
+        city: "",
+        country: "",
+        humidity: "",
+        description: "",
+        error: "Please Enter Data"
+      });
+    }
+    console.log("weatherData", weatherData);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="form-container">
+        <Form getWeather={getWeather} />
+        <Weather
+          tempreature={weatherData.tempreature}
+          city={weatherData.city}
+          country={weatherData.country}
+          humidity={weatherData.humidity}
+          description={weatherData.description}
+          error={weatherData.error}
+        />
+      </div>
     </div>
   );
 }
-
-export default App;
